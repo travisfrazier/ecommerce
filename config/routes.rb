5bb003ebc30bc2193fca235fe393e5b3029e3373
travisfrazier <travisf777@gmail.com>
2017-12-04 10:33:11 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  get 'categorical' => 'storefron#items_by_category'
+
+  get 'branding' => 'storefront#items_by_brand'
+
+  resources :categories
+  resources :products
+
+  root 'storefront#all_items'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
